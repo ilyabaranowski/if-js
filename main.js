be4b2e508c75ed78e718ea3e8cfcaa19645ef031
unknown <iliuxa32@gmail.comm>
2021-08-12 00:27:17 +0300

    lesson-12 done

--- main.js
+++ main.js
@@ -1,89 +1,45 @@
-const data = [
-  {
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
-  },
-  {
-    name: 'Apartment Sunshine',
-    city: 'Santa  Cruz de Tenerife',
-    country: 'Spain',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
-  },
-  {
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
-  },
-  {
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-  },
-  {
-    name: 'Radisson Blu Hotel',
-    city: 'Kyiv',
-    country: 'Ukraine',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
-  },
-  {
-    name: 'Paradise Hotel',
-    city: 'Guadalupe',
-    country: 'Mexico',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
-  },
-  {
-    name: 'Hotel Grindewald',
-    city: 'Interlaken',
-    country: 'Switzerland',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-  },
-];
+const dataLoves = async () => {
+  const dataMain = await fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
+    .then(response => response.json())
+    .then((data) => data);
 
-const wrapper = document.querySelector('body');
+  const wrapper = document.querySelector('body');
 
-const sectionLoves = document.createElement('section');
-sectionLoves.classList.add('loves');
-wrapper.prepend(sectionLoves);
+  const sectionLoves = document.createElement('section');
+  sectionLoves.classList.add('loves');
+  wrapper.prepend(sectionLoves);
 
-const lovesH2 = document.createElement('h2');
-const lovesNode = document.createTextNode('Homes guests loves');
-lovesH2.prepend(lovesNode);
-sectionLoves.prepend(lovesH2);
-lovesH2.classList.add('loves');
-const lovesSlider = document.createElement('div');
-lovesSlider.classList.add('loves__items');
-lovesH2.after(lovesSlider);
+  const lovesH2 = document.createElement('h2');
+  const lovesNode = document.createTextNode('Homes guests loves');
+  lovesH2.prepend(lovesNode);
+  sectionLoves.prepend(lovesH2);
+  lovesH2.classList.add('loves');
+  const lovesSlider = document.createElement('div');
+  lovesSlider.classList.add('loves__items');
+  lovesH2.after(lovesSlider);
 
-data.slice(0, 4).forEach((item) => {
-  const div = document.createElement('div');
-  lovesSlider.append(div);
+  dataMain.slice(0, 4).forEach((item) => {
+    const div = document.createElement('div');
+    lovesSlider.append(div);
 
-  const a = document.createElement('a');
-  a.setAttribute('src', '#');
-  div.prepend(a);
+    const a = document.createElement('a');
+    a.setAttribute('src', '#');
+    div.prepend(a);
 
-  const img = document.createElement('img');
-  img.setAttribute('src', `${item.imageUrl}`);
-  img.setAttribute('alt', `${item.name}`);
-  a.append(img);
+    const img = document.createElement('img');
+    img.setAttribute('src', `${item.imageUrl}`);
+    img.setAttribute('alt', `${item.name}`);
+    a.append(img);
 
-  const pHostel = document.createElement('p');
-  pHostel.innerHTML = `${item.name}`;
-  a.append(pHostel);
+    const pHostel = document.createElement('p');
+    pHostel.innerHTML = `${item.name}`;
+    a.append(pHostel);
 
-  const pCityCountry = document.createElement('p');
-  pCityCountry.innerHTML = `${item.city}`
-    + ', '
-    + `${item.country}`;
-  pHostel.after(pCityCountry);
-});
+    const pCityCountry = document.createElement('p');
+    pCityCountry.innerHTML = `${item.city}`
+      + ', '
+      + `${item.country}`;
+    pHostel.after(pCityCountry);
+  });
+}
+dataLoves()
